d6a89c70c69763737a3d5f4197e0dbc045959326
yanonjava <yansilv303@gmail.com>
2026-09-08 05:36:23 -0300

    feat: adding neural layers

--- src/minigrad/__init__.py
+++ src/minigrad/__init__.py
@@ -1,8 +1,11 @@
 from cProfile import label
 import math
+
+from numpy.random import random
 import matplotlib.pyplot as plt
 import numpy as np
 from graphviz import Digraph, view
+import torch
 
 
 def trace(root):
@@ -37,7 +40,29 @@ def draw_dot(root):
         dot.edge(str(id(n1)), str(id(n2)) + n2._op)
     dot.render("graph", view=True)
 
-
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(np.random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(np.random.uniform(-1,1))
+    def __call__(self, x):
+        #w * x + b inferencia ou foward pass
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b) 
+        out = act.tanh()
+        return out 
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range (len(nouts))]
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 class Value:
     def __init__(self, data, _children=(), _op="", label=""):
         self.data = data
@@ -59,6 +84,8 @@ class Value:
             other.grad += 1.0 * out.grad
         out._backward = _backward
         return out
+    def __radd__(self, other):
+        return self + other
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -143,8 +170,28 @@ def neuronLayer():
     o = (e - 1) / (e + 1)
     o.backward()
     draw_dot(o)
+def torchNeuronImpl():
+    x1 = torch.Tensor([2.0]).double(); x1.requires_grad = True    
+    x2 = torch.Tensor([0.0]).double(); x2.requires_grad = True
+    w1 = torch.Tensor([-3.0]).double(); w1.requires_grad = True
+    w2 = torch.Tensor([1.0]).double(); w2.requires_grad = True
+    b = torch.Tensor([6.8813735870195432]).double(); b.requires_grad = True
+    n = x1*w1 + x2*w2 + b
+    o = torch.tanh(n)
+    o.backward()
+    print('______')
+    print('x2', x2.grad.item())
+    print('w2', w2.grad.item())
+    print('x1', x1.grad.item())
+    print('w1', w1.grad.item())
+def forwardingNeurons():
+    x = [2.0, 3.0, -1.0]
+    n = MLP(3, [4,4,1]) 
+    o = n(x)
+    print(o)
 def main():
-    neuronLayer()
-
+    #neuronLayer()
+    #torchNeuronImpl()
+    forwardingNeurons()
 if __name__ == "__main__":
     main()
